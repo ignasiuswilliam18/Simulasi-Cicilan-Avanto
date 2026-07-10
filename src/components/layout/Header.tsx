3a3d40c485ab998f23eabb99912adfb8bdb1534d
ignasiuswilliam18 <ignasiuswilliam18@gmail.com>
2026-07-10 12:40:21 +0700

    fix: ubah ukuran OPPO logo di header menjadi h-16 agar lebih besar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -28,7 +28,7 @@ export default function Header({
         <img
           src={oppoLogo}
           alt="OPPO"
-          className="mt-6 h-10 w-auto object-contain"
+          className="mt-6 h-16 w-auto object-contain"
         />
 
       </div>
